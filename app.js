726f49131d17a1bbbac5f385f0b05bdfa9d200a3
WillBMease <will@jellyvibes.com>
2016-08-21 13:26:22 -0700

    adding janets files

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var io = require('socket.io')(http) ;
 
 app.set('view engine', 'ejs');
 
-http.listen(8888, function(){
-    console.log('listening on *:8888');
+http.listen(8887, function(){
+    console.log('listening on *:8887');
 });
 
 
