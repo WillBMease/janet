49af25954bf67bb9eaca56ff109c7b243102c2cf
William Mease <w@rageon.com>
2016-10-29 12:52:25 -0700

    d

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var io = require('socket.io')(http) ;
 
 app.set('view engine', 'ejs');
 
-http.listen(8887, function(){
-    console.log('listening on *:8887');
+http.listen(80, function(){
+    console.log('listening on *:80');
 });
 
 
